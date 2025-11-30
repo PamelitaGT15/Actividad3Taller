dff4b3e096cb7026f96be78f08eb22090d224ffd
Pamela Gonzalez <a01825871@tec.mx>
2025-11-29 23:15:42 -0600

    Create PruebaBasicaTest.java

---
+++ src/main/java/com/thermoaire/agenda/src/test/java/com/thermoaire/agenda/PruebaBasicaTest.java
@@ -0,0 +1,12 @@
+package com.thermoaire.agenda;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PruebaBasicaTest {
+
+    @Test
+    public void testSumaBasica() {
+        assertEquals(5, 2 + 3);
+    }
+}
